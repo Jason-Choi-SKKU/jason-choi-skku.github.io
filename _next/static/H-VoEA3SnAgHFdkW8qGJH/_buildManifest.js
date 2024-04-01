68cea783e8b52d8969fff20862112f7a2f7e9690
jiwnchoi <jiwnchoi@users.noreply.github.com>
2024-04-01 15:25:06 +0000

    deploy: 2f83104a45b65b49c6242342861c19be20dc0115

---
+++ _next/static/H-VoEA3SnAgHFdkW8qGJH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/04020a7d-13ea0546f3a122a8.js","static/chunks/115-0547c28ba2a4a52f.js","static/chunks/pages/index-7384981cf107e2d4.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/UZhMNJRe_f6isPSYjV1B3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/04020a7d-13ea0546f3a122a8.js","static/chunks/115-0547c28ba2a4a52f.js","static/chunks/pages/index-95033358c1257cb5.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
