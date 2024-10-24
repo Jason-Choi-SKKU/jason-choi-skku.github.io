fbf3654dc3c45746735fa2cd725b7e0cf6ff12b1
jiwnchoi <jiwnchoi@skku.edu>
2024-10-25 00:06:29 +0900

    fix hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Providers from "./providers"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Providers>{children}</Providers>
